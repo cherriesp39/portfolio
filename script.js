158bb5ebefe45c7f934ea43940b310afdb74225d
Sakura <s4088734@student.rmit.edu.au>
2026-05-27 11:32:49 +1000

    bunch of stuff

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const lightSwitch = document.getElementById("lightson");
+const sunMoon = document.getElementById("lightonimg");
 const mainContent = document.getElementById("maincontent");
+const flowers = document.getElementById("headerimg");
+var loadScreen = document.getElementById("loading");
+let night = true;
 
+window.addEventListener("load", function() {
+    loadScreen.style.display = "none"
+ }
+)
 lightSwitch.addEventListener("click", () => {
-    mainContent.classList.toggle("lightmain");
-})
+    mainContent.classList.toggle("lightmain")
+    }
+)
+lightSwitch.addEventListener("click", function(){
+    sunMoon.src = "./images/icons/moon.png",
+    night = !night;
+    if (night) {
+        sunMoon.src = "./images/icons/sun.png"
+    } else {
+        sunMoon.src = "./images/icons/moon.png"
+    }
+});
